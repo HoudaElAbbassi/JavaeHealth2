cbc4190a36610cf1f6c42b66d91e81a4c6f36d1f
ahmedagdmoun <98025282+ahmedagdmoun@users.noreply.github.com>
2022-01-21 12:31:01 +0100

    Update Mailer.java

--- src/utilities/Mailer.java
+++ src/utilities/Mailer.java
@@ -11,9 +11,7 @@ public class Mailer {
     public static void sendMail(String recipient, String msg, String subject) throws MessagingException {
         System.out.println("Preparing...");
         Properties props = new Properties();
-        props.put("mail.smtp.socketFactory.port", "587");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.transport.protocol", "smtps");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
