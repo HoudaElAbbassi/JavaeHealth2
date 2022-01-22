6870248d16ba64d7e08d1c59075cd6c6eb6ae7af
s9197739 <s9197739@stud.uni-frankfurt.de>
2022-01-22 15:20:03 +0100

    changed

--- src/Testing.java
+++ src/Testing.java
@@ -1,13 +1,19 @@
 import GUI.MainPage;
 import GUI.homePage.patient.PatientHomePage;
+import appointments.Appointment;
+import appointments.AppointmentDAOImp;
 import user.Patient.PatientDAOImp;
 
 
 public class Testing {
 
     public static void main(String[] args){
-        MainPage mainPage = new MainPage();
-        mainPage.setVisible(true);
+        //MainPage mainPage = new MainPage();
+        //mainPage.setVisible(true);
+
+        AppointmentDAOImp appointmentDAOImp=new AppointmentDAOImp();
+        Appointment a=appointmentDAOImp.getAppointmentByDoctorId(2);
+        System.out.println(a.getHealthInfo());
     }
 
 }
